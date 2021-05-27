2fe8f9fdc603dc1fae93503a0cd82cce02b9293c
Angel-Davila <a6519150005@utch.edu.mx>
2021-05-26 19:14:36 -0600

    updating Application javascript

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom"
-import Badge from './components/Badge.jsx';
+import Badge from "./components/Badge.jsx";
 import Layout from "./pages/layout/Layout";
 import Main from "./pages/main/Main"; 
 import NewBadge from "./pages/NewBadge/NewBadge"
+import NotFound from "./components/NotFound.jsx"
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Main}></Route>
           <Route exact path="/badges" component={Badge}></Route>
-          <Route exact path="/new" component={NewBadge}></Route>
+          <Route exact path="/new" component={NewBadge}></Route>          
+          <Route component={NotFound}></Route>
         </Switch>
       </Layout>
     </BrowserRouter>
@@ -20,4 +22,3 @@ function App() {
 }
 
 export default App;
- 
