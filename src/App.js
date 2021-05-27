332a82c3e62d4ac322196167ad986d8df1aaff05
Angel-Davila <a6519150005@utch.edu.mx>
2021-05-26 23:57:40 -0600

    path to login and signin added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Badge from "./components/Badge.jsx";
 import Layout from "./pages/layout/Layout";
 import Main from "./pages/main/Main"; 
 import NewBadge from "./pages/NewBadge/NewBadge"
-import NotFound from "./components/NotFound.jsx"
+import NotFound from "./components/NotFound"
+import Login from "./pages/Login/Login"
+import SignIn from "./pages/SignIn/SignIn"
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
           <Route exact path="/" component={Main}></Route>
           <Route exact path="/badges" component={Badge}></Route>
           <Route exact path="/new" component={NewBadge}></Route>          
+          <Route exact path="/login" component={Login}></Route>          
+          <Route exact path="/signin" component={SignIn}></Route>        
           <Route component={NotFound}></Route>
         </Switch>
       </Layout>
